d6cd4faf75e21c4f1030290e58ed0163fa2f1ee7
alejandrosanchezr2023 <alejosanchez82@icloud.com>
2023-06-28 20:38:50 -0500

    app.ts: Agregamos tipos a variables de Express

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,19 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
 
 const app = express ();
 //**routing: enrutamiento */
 //http://localhost:3000/
 
-app.get( '/', ( req, res ) => {
+app.get( '/', ( req : Request, res: Response ) => {
  const message = 'Bienvenido a la API de Ecommerce';
    
  console.log(message);// mensaje a la terminal
  
  res.send(`<h1>${message}</h1>`); // mensaje al navegador
 });
-//http://localhost:3000/Home
-app.get('/home',(req, res) => {
- const namePage = 'Home';
+//http://localhost:3000/Home (EndPoint)
+app.get('/home',(req : Request, res: Response) => {
+ const namePage: string = 'Home';
 
 
  console.log(namePage);
