234bc8957e11d266624708f0a6318389e2c72723
alejandrosanchezr2023 <alejosanchez82@icloud.com>
2023-06-28 20:30:14 -0500

    app.ts: define 2 endpoints; / , / Home

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,25 @@
 import express from 'express';
 
 const app = express ();
+//**routing: enrutamiento */
+//http://localhost:3000/
+
+app.get( '/', ( req, res ) => {
+ const message = 'Bienvenido a la API de Ecommerce';
+   
+ console.log(message);// mensaje a la terminal
+ 
+ res.send(`<h1>${message}</h1>`); // mensaje al navegador
+});
+//http://localhost:3000/Home
+app.get('/home',(req, res) => {
+ const namePage = 'Home';
+
+
+ console.log(namePage);
+ res.send(`<h1>${namePage}</h1>`)
+ 
+});
 
 app.listen(3000, () => {
     console.log('servidor lanzado en el puerto 3000');
