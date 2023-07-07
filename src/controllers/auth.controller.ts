725affb4938568b486a6abfd423e21d08c0aa12c
alejandrosanchezr2023 <alejosanchez82@icloud.com>
2023-07-06 21:54:55 -0500

    auth.service. auth.controller: Vinculamos service con el controlador de auth

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,12 +1,24 @@
+
 import { Request, Response } from "express";
+import { registerNewUser } from "../services/auth.service";
 
 
 
-const register = ( req: Request, res: Response ) => {
-    console.log( 'Registra usuario' );
-    res.send( 'Registra usuario' );
+const register = async ( req: Request, res: Response ) => {
+    const user = req.body;
+
+    try {
+        const response = await registerNewUser ( user );
+        res.json(response);
+
+    } catch (error) {
+        console.log( `error en el registro del usuario` );
+    res.json ({
+    msg:'ERROR_REGISTER_USER'
+    });
+  }
+  
 }
-
     
 const login = ( req: Request, res: Response ) => {
     console.log( 'Login usuario' );
